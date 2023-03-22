54b3f5dcd6d3557acbeb6d9f93d3651fcf035a37
Takkino31 <takkinoyaya@gmail.com>
2023-03-22 13:03:25 +0000

    les variables 001

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const b = 'age'
+
+const element = {
+    nom : 'aîcha',
+    prenom: 'TextDecodeStream',
+    [b] :2,
+    notes : 
+        {
+            anglais : 12,
+            fr: 12,
+            eps: [10,12,34]
+        }
+}
+
+console.log({element});
+console.log(element.notes);
